ac7ab929889e34feadaf0095461fcbf84ac21185
danyuanfei221 <178731754@qq.com>
2018-03-12 10:22:21 +0800

    first commit

---
+++ app/src/main/java/com/yuanfei/selectpicture/MainActivity.java
@@ -0,0 +1,8 @@
+package com.yuanfei.selectpicture;
+
+/**
+ * Created by yuanfei on 2018/3/12.
+ */
+
+public class MainActivity {
+}
---
+++ picture_library/src/main/java/com/yuanfei/picture_lib/PictureFileProvider.java
@@ -0,0 +1,10 @@
+package com.yuanfei.picture_lib;
+
+import android.support.v4.content.FileProvider;
+
+/**
+ * Created by Mankin on 2017/8/29.
+ */
+
+public class PictureFileProvider extends FileProvider {
+}
